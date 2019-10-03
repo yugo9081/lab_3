ebc44af2b9a7e472b66966199d87cba70beae07b
Sally Go <54964332+yugo9081@users.noreply.github.com>
2019-10-02 22:52:25 -0600

    Add files via upload

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Authors : Yubin Go, Yifan Chen, Daniel Kim
 # Date: 9/19/2019
-
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
 
@@ -10,5 +9,9 @@ read filename
 echo "Enter a regular expression: "
 read exp
 
-grep $exp $filename
-
+grep $exp $filename 
+echo "List of 303 area code phone numbers:"
+egrep "^303-[0-9]{3}-[0-9]{4}$"  $filename
+echo "Number of emails:"
+grep -c "@" $filename
+grep “@geocities.com” $filename >>email_results.txt
